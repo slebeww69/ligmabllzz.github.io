3c0243251f8ddb3448b1f682ed578f41bab512b6
slebeww69 <ferickhanderson04@gmail.com>
2023-10-04 08:20:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".carousel-inner, .carousel-item", {
+  origin: "top",
+});
+
+ScrollReveal().reveal(".shoes-image", {
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".arrival-section", {
+  origin: "left",
+});
+
+ScrollReveal().reveal(".top-seller", {
+  origin: "right",
+});
